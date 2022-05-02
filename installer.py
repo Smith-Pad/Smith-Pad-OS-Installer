621c8c5d177b7ef093c70b4f741995b6f81484cf
senalbulumulle <senal.bulumulle@gmail.com>
2022-05-02 09:59:24 -0400

    Removed module: archinstall

--- installer.py
+++ installer.py
@@ -2,9 +2,6 @@ import logging
 import os
 import time
 import parted
-import archinstall
-from archinstall import ConfigurationOutput
-from archinstall.lib.models.network_configuration import NetworkConfigurationHandler
 def welcomeBanner(): 
 
         print("""

9da446cd4f079901bc6240667f1eb86d5d505835
senalbulumulle <senal.bulumulle@gmail.com>
2022-05-04 15:21:23 -0400

    For now, ask the user if they want to format the disk.

--- installer.py
+++ installer.py
@@ -13,6 +13,18 @@ def initialize():
 def partitionDisk(): 
         # listDisks = os.system("lsblk -d | grep disk")
 
+        ## For now, ask the user if they want to format the disk.
+        before_formatting_disk = input("Are you sure you want to format the disk? |yes| |no|\n")
+
+        ## The disk will be formatted if the user enters yes.  
+        if before_formatting_disk == "yes": 
+                formatDisk = os.system("sh scripts/formatDisk.sh")
+
+        ##  If the user enters no, the program will be terminated.
+        elif before_formatting_disk == "no":
+                exit()
+
+
 welcomeBanner()
 initialize()
 partitionDisk()

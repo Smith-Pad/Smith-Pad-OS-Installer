209907f54f0f04169ac82152522d7e00e4042235
senalbulumulle <senal.bulumulle@gmail.com>
2022-05-04 11:00:59 -0400

    Created function called initialize

--- installer.py
+++ installer.py
@@ -4,4 +4,11 @@ def welcomeBanner():
         print ("Welcome to the Smith-Pad-OS Installer")
 
 
+def initialize():
+        connectNetwork = os.system("sh scripts/connectNetwork.sh")
+        updateSystemClock = os.system("sh scripts/updateSystemClock.sh")
+
+
+
 welcomeBanner()
+initialize()

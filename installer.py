4d5cd58cdb0c743fff600ecc9258e20ccc7218f8
senalbulumulle <senal.bulumulle@gmail.com>
2022-05-05 09:25:28 -0400

    Changed variable from before_formatting_disk to confirmDisk

--- installer.py
+++ installer.py
@@ -14,14 +14,14 @@ def partitionDisk():
         # listDisks = os.system("lsblk -d | grep disk")
 
         ## For now, ask the user if they want to format the disk.
-        before_formatting_disk = input("Are you sure you want to format the disk? |yes| |no|\n")
+        confirmDisk = input("Are you sure you want to format the disk? |yes| |no|\n")
 
         ## The disk will be formatted if the user enters yes.  
-        if before_formatting_disk == "yes": 
+        if confirmDisk == "yes": 
                 formatDisk = os.system("sh scripts/formatDisk.sh")
 
         ##  If the user enters no, the program will be terminated.
-        elif before_formatting_disk == "no":
+        elif confirmDisk == "no":
                 exit()
 
 

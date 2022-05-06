4097f15b0c0253c51c55997be36002278ae78ad4
senalbulumulle <senal.bulumulle@gmail.com>
2022-05-06 13:00:09 -0400

    Commented some things../..

--- scripts/bootMount.sh
+++ scripts/bootMount.sh
@@ -2,7 +2,10 @@
 #		bootMount.sh
 ############################################################
 
-
+## After booting the chroot, it will install all  
+## the packages provided here and other things 
+## before rebooting the system. 
+##
 
 arch-chroot /mnt/ /bin/bash <<"EOT"
 

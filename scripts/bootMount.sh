bdef2e232167a421dd2d8b1cfaf98837a440479a
senalbulumulle <senal.bulumulle@gmail.com>
2022-05-07 08:45:39 -0400

    Removed the functions to see it works...

--- scripts/bootMount.sh
+++ scripts/bootMount.sh
@@ -8,22 +8,14 @@
 ##
 
 arch-chroot /mnt/ /bin/bash <<"EOT"
-	function installPackages() {
-		pacman -Sy
-		pacman -S git
-		pacman -S gnome-tweaks 
-		pacman -S pcmanfm 
-		pacman -S xorg-server 
-	}
+	pacman -Sy
+	pacman -S git
+	pacman -S gnome-tweaks 
+	pacman -S pcmanfm 
+	pacman -S xorg-server 
 
 
-	function prompt_user_hostname() {
-		echo "Please enter the device name: \n"
-		read $hostname
-		echo $hostname >> /etc/hostname
-	}
 
-	installPackages
-	prompt_user_hostname
+	echo $hostname >> /etc/hostname
 	
 EOT
